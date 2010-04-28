7edf8af1c75c76f033643d0394253b343ccad4b4
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2010-04-28 12:19:08 +0000

    adding of new form to svn. Moving this form to correct project

---
+++ dev/FilterSimulationWithTablesAndGraphs/YAxisListingForm.Designer.cs
@@ -0,0 +1,100 @@
+namespace FilterSimulation
+{
+    partial class YAxisListingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.okButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.okButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 256);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(197, 23);
+            this.panel1.TabIndex = 0;
+            // 
+            // okButton
+            // 
+            this.okButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.okButton.Location = new System.Drawing.Point(122, 0);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkedListBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(197, 256);
+            this.panel2.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(197, 244);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // YAxisListingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 279);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "YAxisListingForm";
+            this.Text = "YAxisListingForm";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
---
+++ dev/FilterSimulationWithTablesAndGraphs/YAxisListingForm.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FilterSimulation
+{
+    public partial class YAxisListingForm : Form
+    {
+        public YAxisListingForm()
+        {
+            InitializeComponent();
+
+            checkedListBox1.Items.Clear();
+            foreach (fmCalculationLibrary.fmGlobalParameter parameter in fmCalculationLibrary.fmGlobalParameter.Parameters)
+            {
+                checkedListBox1.Items.Add(parameter.name);
+            }
+        }
+
+        public void CheckItems(List<fmCalculationLibrary.fmGlobalParameter> list)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; ++i)
+                if (list.Contains(GetParameterInListBoxByIndex(i)))
+                    checkedListBox1.SetItemChecked(i, true);
+        }
+
+        public List<fmCalculationLibrary.fmGlobalParameter> GetCheckedItems()
+        {
+            List<fmCalculationLibrary.fmGlobalParameter> result = new List<fmCalculationLibrary.fmGlobalParameter>();
+            for (int i = 0; i < checkedListBox1.Items.Count; ++i)
+                if (checkedListBox1.GetItemChecked(i))
+                    result.Add(GetParameterInListBoxByIndex(i));
+            return result;
+        }
+
+        private fmCalculationLibrary.fmGlobalParameter GetParameterInListBoxByIndex(int i)
+        {
+            return fmCalculationLibrary.fmGlobalParameter.ParametersByName[checkedListBox1.Items[i] as string];
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
