46336c1ad4cb267edfd1fa38ae783034b6b29927
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2010-10-04 13:37:01 +0000

    auto adding of releases to SVN

--- dev/Releases/Package.bat
+++ dev/Releases/Package.bat
@@ -1,1 +1,2 @@
-7z a AdvancedCalculator.1.0.199.0.7z "..\AdvancedCalculator\bin\Release\*.*"
+7z a AdvancedCalculator.1.0.201.0.7z "..\AdvancedCalculator\bin\Release\*.*"
+svn add AdvancedCalculator.1.0.201.0.7z
