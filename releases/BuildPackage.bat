6d714f10d6ee12e4a68afdaea09976445ee52d6e
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2010-10-04 14:05:26 +0000

    to rev 206

--- releases/BuildPackage.bat
+++ releases/BuildPackage.bat
@@ -1,3 +1,3 @@
 C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild ..\dev\AdvancedCalculator.sln /t:Rebuild /p:Configuration=Release
-SubWCRev ..\ Package.template.bat Package.bat
+SubWCRev ..\dev\ Package.template.bat Package.bat
 Package.bat
--- releases/Package.bat
+++ releases/Package.bat
@@ -1,2 +1,2 @@
-7z a AdvancedCalculator.1.0.205.0.7z "..\dev\AdvancedCalculator\bin\Release\*.*"
-svn add AdvancedCalculator.1.0.205.0.7z
+7z a AdvancedCalculator.1.0.206.0.7z "..\dev\AdvancedCalculator\bin\Release\*.*"
+svn add AdvancedCalculator.1.0.206.0.7z
