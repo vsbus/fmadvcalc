7bb19d036089d6c144e96e0e30008f7e700c75e6
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2010-10-06 11:42:51 +0000

    improve package creating

--- releases/Package.template.bat
+++ releases/Package.template.bat
@@ -1,2 +1,3 @@
-7z a AdvancedCalculator.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\*.*"
+7z a AdvancedCalculator.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\AdvancedCalculator.exe"
+7z a AdvancedCalculator.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\*.dll"
 svn add AdvancedCalculator.1.0.$WCREV$.0.7z
