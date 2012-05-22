38c33d799140ea6790f5a975d0beaddacb2462b8
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2012-05-22 20:58:28 +0000

    FILTRAPLUS

--- releases/Package.template.bat
+++ releases/Package.template.bat
@@ -1,3 +1,3 @@
-7z a AdvancedCalculator.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\AdvancedCalculator.exe"
+7z a AdvancedCalculator.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\FILTRAPLUS.exe"
 7z a AdvancedCalculator.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\*.dll"
 svn add AdvancedCalculator.1.0.$WCREV$.0.7z
