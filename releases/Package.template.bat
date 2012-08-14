57af6f53519c48442e2e74ca599e9b93d14e2787
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2012-08-14 17:07:41 +0000

    release build update/fix

--- releases/Package.template.bat
+++ releases/Package.template.bat
@@ -1,3 +1,4 @@
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\FILTRAPLUS.exe"
+7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\FiltrationCalculator.exe"
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\*.dll"
 svn add FILTRAPLUS.1.0.$WCREV$.0.7z
