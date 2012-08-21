abfe9d7321604b721e8de7f4f9e2e3787e66221d
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2012-08-21 20:39:29 +0000

    fmConvert

---
+++ dev/fmMisc/fmConvert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fmMisc
+{
+    public class fmConvert
+    {
+        public static double ToDouble(string s)
+        {
+            string decimalSeparator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            s = s.Replace('.', decimalSeparator[0]);
+            s = s.Replace(',', decimalSeparator[0]);
+            return Convert.ToDouble(s);
+        }
+    }
+}
