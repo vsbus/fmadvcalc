2640801fe6e3fa0316606fc0f0c28b7b33c292e3
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2013-01-03 06:12:46 +0000

    filtraplus.cfg and other DataFiles will be added to archive.

--- releases/Package.template.bat
+++ releases/Package.template.bat
@@ -1,4 +1,5 @@
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\FILTRAPLUS.exe"
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\FiltrationCalculator.exe"
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\*.dll"
+7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\DataFiles\*.*"
 svn add FILTRAPLUS.1.0.$WCREV$.0.7z
