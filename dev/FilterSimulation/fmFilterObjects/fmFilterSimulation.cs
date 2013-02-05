5c43585c66397ead400f7ce495f590d777fe3694
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2013-02-05 06:06:27 +0000

    default values for the new simulation added: eta_g = 0.02; ag1 = ag2 = ag3 = 0; Tetta = 20;

--- dev/FilterSimulation/fmFilterObjects/fmFilterSimulation.cs
+++ dev/FilterSimulation/fmFilterObjects/fmFilterSimulation.cs
@@ -792,10 +792,18 @@ namespace FilterSimulation.fmFilterObjects
                 ((fmCalculationVariableParameter) Data.parameters[var.globalParameter]).isInputed = var.isInputed;
             }
 
+
+            Data.parameters[fmGlobalParameter.eta_g].value = new fmValue(0.02);
+            Data.parameters[fmGlobalParameter.ag1].value = new fmValue(0);
+            Data.parameters[fmGlobalParameter.ag2].value = new fmValue(0);
+            Data.parameters[fmGlobalParameter.ag3].value = new fmValue(0);
+            Data.parameters[fmGlobalParameter.Tetta].value = new fmValue(20);
+
             Data.parameters[fmGlobalParameter.Tetta_boil].value = new fmValue(1000);
             Data.parameters[fmGlobalParameter.DH].value = new fmValue(1e6);
             Data.parameters[fmGlobalParameter.Mmole].value = new fmValue(18e-3);
             Data.parameters[fmGlobalParameter.f].value = new fmValue(1);
+
 
             Keep();
         }
