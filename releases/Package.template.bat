3b6f680a14b07fabaedba9f3f82bb05ef95c20fd
victorbarinov <victorbarinov@5d4324d9-edda-0289-c489-45457e431a19>
2013-02-06 16:28:05 +0000

    disable auto adding to svn packages

--- releases/Package.template.bat
+++ releases/Package.template.bat
@@ -2,4 +2,3 @@
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\FiltrationCalculator.exe"
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\AdvancedCalculator\bin\Release\*.dll"
 7z a FILTRAPLUS.1.0.$WCREV$.0.7z "..\dev\DataFiles\*.*"
-svn add FILTRAPLUS.1.0.$WCREV$.0.7z
